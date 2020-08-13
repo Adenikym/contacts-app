f7c305955f425cfac85ce25cd048a213430efe6b
Adenikym <mm5adeniky@gmail.com>
2020-08-13 18:46:42 +0100

    new

--- contact.js
+++ contact.js
@@ -63,6 +63,40 @@ function deleteContact (contactname) {
     localStorage.setItem("contact", JSON.stringify(contact));
 }
 
+function editContact (contactname) {
+
+    var contact= JSON.parse(localStorage.getItem("contact"));
+
+    for (let i=0; i<contact.length; i++) {
+    document.getElementById("name1").value= contact[i].contactname;
+    document.getElementById("num1").value=contact[i].phonenumber;
+    document.getElementById("mail1").value=contact[i].Email;
+    }
+
+  document.getElementById("subedit").addEventListener("submit",function (contactname) 
+  
+  {
+    var econtact = {
+        contactname:document.getElementById("name1").value,
+        phonenumber:  document.getElementById("num1").value,
+        Email: document.getElementById("mail1").value
+
+
+
+       }
+
+       for(let i=0; i<contact.length; i++){
+        if (contact[i].contactname===contactname){
+            contact.splice(i,1[econtact]);
+        }}
+  })
+
+   
+
+  localStorage.setItem("contact", JSON.stringify(contact)); 
+
+}
+
 function displayContacts() {
     
     var contact= JSON.parse(localStorage.getItem("contact"));
@@ -74,13 +108,17 @@ function displayContacts() {
       var number= contact[i].phonenumber;
       var mail= contact[i].Email;
       var Deletecontact="-";
+      var Editcontact="edit"
 
        saved.innerHTML+='<div class="repeat">'+
                              "<h3>"+name+
                              " </h3>"+
                              "<p>"+number+"</p>"+
                              "<p class='mi'>"+mail+"</p>"+
+                             "<button onclick='editContact(`"+name+"`)' class='del'>"+Editcontact+"</button>"
+                             +'<br>'+
                              "<button onclick='deleteContact(`"+name+"`)' class='del'>"+Deletecontact+"</button>"
                              " </div>";
    }
 }
+
